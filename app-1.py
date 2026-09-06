21cc4c37714a7e3484c67f6d6f13780fa30b681b
irshadsawan <irshad.sawan@gmail.com>
2026-09-06 23:09:47 +0500

    Update app-1.py

--- app-1.py
+++ app-1.py
@@ -161,7 +161,7 @@ with st.sidebar:
     model = st.selectbox(
         "Groq Model",
         [
-            "llama-3.3-70b-versatile",
+            "openai/gpt-oss-120b",
             "llama-3.1-8b-instant",
         ],
     )
